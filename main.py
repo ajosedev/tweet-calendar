e8a43ddb3b8e692e942c0700526b4dd698134cb4
Andrew Jose <ajosedev@gmail.com>
2018-12-21 16:40:32 +1100

    Filter out unwanted tweets

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import json
+import random
+import sys
 from pprint import pprint
 
 # Quote of the day calendar
 
-# Figure out how to shuffle
 # Iterate over every tweet
 # Ignore tweets with links in them?
 
@@ -13,8 +14,33 @@ from pprint import pprint
 # Figure out design for screen
 
 if __name__ == "__main__":
+    verbose = 'verbose' in sys.argv
+    count = 0
     with open('tweets.json') as f:
         tweets = json.load(f)
+        # Shuffle tweets into random order
+        random.shuffle(tweets)
 
         for tweet in tweets:
+
+            # Take out links
+            if 'http' in tweet['full_text']:
+                continue
+
+            # Take out quotes
+            if tweet['is_quote_status']:
+                continue
+
+            # Double check for links and images and such
+            if tweet['entities']['urls'] or 'media' in tweet['entities']:
+                continue
+
+            # Take out replies
+            if tweet['in_reply_to_status_id'] is not None:
+                continue
+
             print(tweet['full_text'])
+            count += 1
+
+        if verbose:
+            print('Count: {}'.format(count))
