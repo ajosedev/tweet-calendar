91e97adcdc7808d9c6838cff65d450172d03841f
Andrew Jose <ajosedev@gmail.com>
2018-12-24 09:12:28 +1100

    Double the size of pixels/fonts
    Hopefully to improve print quality

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ twitter_name = 'Thoughts of dog'
 twitter_handle = 'dog_feelings'
 
 # Fonts
-date_num_font = ImageFont.truetype('/Users/andrew/Library/Fonts/Roboto-Black.ttf', 100)
-date_day_font = ImageFont.truetype('/Users/andrew/Library/Fonts/Roboto-Bold.ttf', 40)
-tweet_heading_font = ImageFont.truetype('/Users/andrew/Library/Fonts/Roboto-Bold.ttf', 30)
-tweet_body_font = ImageFont.truetype('/Users/andrew/Library/Fonts/Roboto-Regular.ttf', 30)
+date_num_font = ImageFont.truetype('/Users/andrew/Library/Fonts/Roboto-Black.ttf', 200)
+date_day_font = ImageFont.truetype('/Users/andrew/Library/Fonts/Roboto-Bold.ttf', 80)
+tweet_heading_font = ImageFont.truetype('/Users/andrew/Library/Fonts/Roboto-Bold.ttf', 60)
+tweet_body_font = ImageFont.truetype('/Users/andrew/Library/Fonts/Roboto-Regular.ttf', 60)
 
 # Colours
 colour_date_num = (0, 0, 0)
@@ -29,23 +29,23 @@ colour_body = (20, 23, 26)
 
 def write_tweet(date, tweet):
     avatar = Image.open('avatar.png')
-    txt = Image.new('RGBA', (750, 500), (255, 255, 255, 0)) # Change this to 255, 255, 255, 0 when saving
+    txt = Image.new('RGBA', (1400, 1000), (255, 255, 255, 0)) # Change this to 255, 255, 255, 0 when saving
     draw = ImageDraw.Draw(txt)
-    draw.rectangle([(1, 1), (749, 499)], fill=None, outline='black')
+    draw.rectangle([(1, 1), (1399, 999)], fill=None, outline='black')
 
     # Set start 'margins'
-    y_start = 30
-    x_text = 165
-    y_text = y_start + 180
+    y_start = 60
+    x_text = 330
+    y_text = y_start + 360
 
     # Write date
-    draw.text((40, y_start), date.strftime('%d'), fill=colour_date_num, font=date_num_font, align='right')
-    draw.text((x_text, y_start + 15), date.strftime('%A'), fill=colour_date_day, font=date_day_font)
-    draw.text((x_text, y_start + 55), date.strftime('%b'), fill=colour_date_day, font=date_day_font)
+    draw.text((80, y_start), date.strftime('%d'), fill=colour_date_num, font=date_num_font, align='right')
+    draw.text((x_text, y_start + 30), date.strftime('%A'), fill=colour_date_day, font=date_day_font)
+    draw.text((x_text, y_start + 110), date.strftime('%b'), fill=colour_date_day, font=date_day_font)
 
     # Write tweet header
-    draw.text((x_text, y_start + 145), '{}'.format(twitter_name), fill=colour_body, font=tweet_heading_font)
-    draw.text((395, y_start + 145), '@{}'.format(twitter_handle), fill=colour_user, font=tweet_body_font)
+    draw.text((x_text, y_start + 290), '{}'.format(twitter_name), fill=colour_body, font=tweet_heading_font)
+    draw.text((790, y_start + 290), '@{}'.format(twitter_handle), fill=colour_user, font=tweet_body_font)
 
     # Write tweet content
     lines = textwrap.wrap(tweet, width=35)
@@ -58,7 +58,7 @@ def write_tweet(date, tweet):
 
     # Save image
     print('Saving...')
-    txt.paste(avatar, (42, y_start + 145)) # Assumes the avatar is styled correctly
+    txt.paste(avatar, (84, y_start + 290)) # Assumes the avatar is styled correctly
     txt.save('images/{}.png'.format(date.strftime('%d-%m')))
 
 if __name__ == "__main__":
