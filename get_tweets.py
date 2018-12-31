dde9faed554de5f380e450fd6253a0caf8347183
Andrew <ajosedev@gmail.com>
2018-12-31 19:05:37 +1100

    Update variables for consistency

--- get_tweets.py
+++ get_tweets.py
@@ -7,7 +7,7 @@ auth = tweepy.OAuthHandler(secrets.consumer_key, secrets.consumer_secret)
 auth.set_access_token(secrets.access_token, secrets.access_token_secret)
 api = tweepy.API(auth)
 
-twitter_name = "dog_feelings"
+twitter_handle = "dog_feelings"
 count = 600
 
 # Get a bunch of tweets for a given user
@@ -16,7 +16,7 @@ count = 600
 if __name__ == "__main__":
     json_tweets = []
 
-    for tweet in tweepy.Cursor(api.user_timeline, screen_name = twitter_name, count = count, include_rts = False, tweet_mode = 'extended', trim_user = True).items(500):
+    for tweet in tweepy.Cursor(api.user_timeline, screen_name = twitter_handle, count = count, include_rts = False, tweet_mode = 'extended', trim_user = True).items(500):
         json_tweets.append(tweet._json)
 
     with open('tweets.json', 'w') as outfile:
